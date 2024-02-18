395fc272992e0906547c6e5ba07b729294ebb71a
Philimuhire <philimuhire@gmail.com>
2024-02-19 00:37:15 +0200

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const express = require("express")
     app.get('/', (req, res) => {
         res.send(`Hello NODE API`)
     })
+
+    app.get('/blog', (req, res) => {
+        res.send(`Hello Blog, My name is Philbert. I am learning Node.js`)
+    })
     
     app.listen(3000, () => {
         console.log("Server has started!")
